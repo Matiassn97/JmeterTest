65c85cd2f13b8afca2b5ad012cdaa7b7b74b06c6
Matiassn97 <matias.sanchezn97@gmail.com>
2021-09-16 09:45:37 -0300

    Minimum

--- jmeter-test-resp-seg/monitoring.bat
+++
@@ -1,9 +0,0 @@
-@set INT=%1
-@set LOG=%2
-@set PERERR=%3
-@set REQ=%4
-@echo services: >monitoring.yaml
-@echo - module: monitoring>>monitoring.yaml
-@echo   local: >>monitoring.yaml
-@echo     - interval: %INT%s >>monitoring.yaml
-@echo     - logging: %LOG% >>monitoring.yaml
